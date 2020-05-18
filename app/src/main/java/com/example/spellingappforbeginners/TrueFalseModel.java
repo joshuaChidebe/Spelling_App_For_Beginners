411591feb77c7543b8dc90f0a890886bfdd0143a
Pman <chidebeprince2020@gmail.com>
2020-05-18 12:59:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/spellingappforbeginners/TrueFalseModel.java
@@ -0,0 +1,61 @@
+package com.example.spellingappforbeginners;
+
+public class TrueFalseModel {
+    private int Question;
+    private boolean Answer;
+
+    TrueFalseModel(int question,boolean answer){
+        Question = question;
+        Answer = answer;
+    }
+
+    public int getQuestion() {
+        return Question;
+    }
+
+    public void setQuestion(int question) {
+        Question = question;
+    }
+
+    public boolean isAnswer() {
+        return Answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        Answer = answer;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
